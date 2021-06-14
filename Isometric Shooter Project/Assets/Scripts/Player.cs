b34e9b70833a7a40f409c76dd250368c2dbc6981
aaronmsimon <aaronsimon@gmail.com>
2021-06-13 19:52:00 -0700

    Apply GunController to Player

--- Isometric Shooter Project/Assets/Scripts/Player.cs
+++ Isometric Shooter Project/Assets/Scripts/Player.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(GunController))]
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
     Camera viewCamera;
     PlayerController controller;
+    GunController gunController;
 
     void Start()
     {
         viewCamera = Camera.main;
         controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
     }
 
     void Update()
     {
+        // Movement input
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
 
+        // Look input
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         // Don't need to fetch the ground plane from the game world, but just create it programmatically
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
@@ -33,6 +38,12 @@ public class Player : MonoBehaviour
             // Debug.DrawLine(ray.origin, pointOfIntersection, Color.red);
 
             controller.LookAt(pointOfIntersection);
+        }
+
+        // Weapon input
+        if(Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
         }
     }
 }
