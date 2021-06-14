108541e3e60cade1b39e9718245f5ae7f8fc03a7
aaronmsimon <aaronsimon@gmail.com>
2021-06-13 19:52:34 -0700

    Initial commit

---
+++ Isometric Shooter Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent pathfinder;
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathfinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = .25f;
+        
+        while(target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathfinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
