cd36d9fe380afaf9663168058c6255a62ed6ba8d
aaronmsimon <aaronsimon@gmail.com>
2021-06-14 20:06:00 -0700

    Enemy Attacks

--- Isometric Shooter Project/Assets/Scripts/Enemy.cs
+++ Isometric Shooter Project/Assets/Scripts/Enemy.cs
@@ -6,15 +6,35 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Enemy : LivingEntity
 {
+    public enum State {Idle, Chasing, Attacking};
+    State currentState;
+
     NavMeshAgent pathfinder;
     Transform target;
+    Material skinMaterial;
+
+    Color originalColor;
+
+    float attackDistanceThreshold = .5f;
+    float timeBetweenAttacks = 1f;
+
+    float nextAttackTime;
+    float myCollisionRadius;
+    float targetCollisionRadius;
 
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         pathfinder = GetComponent<NavMeshAgent>();
+        skinMaterial = GetComponent<Renderer>().material;
+        originalColor = skinMaterial.color;
+
+        currentState = State.Chasing;
         target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        myCollisionRadius = GetComponent<CapsuleCollider>().radius;
+        targetCollisionRadius = target.GetComponent<CapsuleCollider>().radius;
 
         StartCoroutine(UpdatePath());
     }
@@ -22,7 +42,46 @@ public class Enemy : LivingEntity
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.time > nextAttackTime)
+        {
+            // Since distance uses square root operation (which is fairly expensive) and we don't need to know the actual distance (just comparing), we can just use their squared form
+            float sqrDistanceToTarget = (target.position - transform.position).sqrMagnitude;
+
+            if(sqrDistanceToTarget < Mathf.Pow(attackDistanceThreshold + myCollisionRadius + targetCollisionRadius,2))
+            {
+                nextAttackTime = Time.time + timeBetweenAttacks;
+                StartCoroutine(Attack());
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        currentState = State.Attacking;
+        pathfinder.enabled = false;
+
+        Vector3 originalPosition = transform.position;
+        Vector3 dirToTarget = (target.position - transform.position).normalized;
+        Vector3 attackPosition = target.position - dirToTarget * (myCollisionRadius);
+
+        float attackSpeed = 3f;
+        float percent = 0f;
+
+        skinMaterial.color = Color.red;
+
+        // Want to lunge at player: original position to player back to the original position - this is parabolic
+        while(percent <= 1)
+        {
+            percent += Time.deltaTime * attackSpeed;
+            float interpolation = 4 * (-Mathf.Pow(percent, 2) + percent);
+            transform.position = Vector3.Lerp(originalPosition, attackPosition, interpolation);
+
+            yield return null;
+        }
+
+        skinMaterial.color = originalColor;
+        currentState = State.Chasing;
+        pathfinder.enabled = true;
     }
 
     IEnumerator UpdatePath()
@@ -31,10 +90,14 @@ public class Enemy : LivingEntity
         
         while(target != null)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
-            if(!dead)
+            if(currentState == State.Chasing)
             {
-                pathfinder.SetDestination(targetPosition);
+                Vector3 dirToTarget = (target.position - transform.position).normalized;
+                Vector3 targetPosition = target.position - dirToTarget * (myCollisionRadius + targetCollisionRadius + attackDistanceThreshold / 2);
+                if (!dead)
+                {
+                    pathfinder.SetDestination(targetPosition);
+                }
             }
             yield return new WaitForSeconds(refreshRate);
         }
