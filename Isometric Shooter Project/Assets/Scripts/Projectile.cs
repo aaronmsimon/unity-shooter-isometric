f55502f30cd97568390af86b2ceeb485bf525e20
aaronmsimon <aaronsimon@gmail.com>
2021-06-15 22:02:44 -0700

    Adjustments for potential glitches
    Potential Glitches (raycasts will not detect a hit):
    1) if spawn point of projectile lies inside of enemy
    2) if frame update includes enemy overlapping projectile

--- Isometric Shooter Project/Assets/Scripts/Projectile.cs
+++ Isometric Shooter Project/Assets/Scripts/Projectile.cs
@@ -8,6 +8,20 @@ public class Projectile : MonoBehaviour
     float speed = 10f;
     float damage = 1f;
 
+    float lifetime = 3f;
+    float skinWidth = .1f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+
+        Collider[] initialCollisions = Physics.OverlapSphere(transform.position, .1f, collisionMask);
+        if(initialCollisions.Length > 0)
+        {
+            OnHitObject(initialCollisions[0]);
+        }
+    }
+
     public void setSpeed(float newSpeed)
     {
         speed = newSpeed;
@@ -26,7 +40,7 @@ public class Projectile : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        if (Physics.Raycast(ray, out hit, moveDistance + skinWidth, collisionMask, QueryTriggerInteraction.Collide))
         {
             OnHitObject(hit);
         }
@@ -34,11 +48,20 @@ public class Projectile : MonoBehaviour
 
     void OnHitObject(RaycastHit hit)
     {
-        // Debug.Log(hit.collider.gameObject.name);
         IDamageable damageableObject = hit.collider.GetComponent<IDamageable>();
         if(damageableObject != null)
         {
             damageableObject.TakeHit(damage, hit);
+        }
+        GameObject.Destroy(gameObject);
+    }
+
+    void OnHitObject(Collider c)
+    {
+        IDamageable damageableObject = c.GetComponent<IDamageable>();
+        if (damageableObject != null)
+        {
+            damageableObject.TakeDamage(damage);
         }
         GameObject.Destroy(gameObject);
     }
