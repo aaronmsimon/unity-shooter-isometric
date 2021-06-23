847e2c2993a3ba024be1336773162521c1846cb9
aaronmsimon <aaronsimon@gmail.com>
2021-06-22 20:37:10 -0700

    Weapon variation

--- Isometric Shooter Project/Assets/Scripts/Gun.cs
+++ Isometric Shooter Project/Assets/Scripts/Gun.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    public Transform muzzle;
+    public enum FireMode
+    {
+        Auto,
+        Burst,
+        Single
+    }
+    public FireMode fireMode;
+
+    public Transform[] projectileSpawn;
     public Projectile projectile;
     public float msBetweenShots = 100f;
     public float muzzleVelocity = 35f;
+    public int burstCount;
 
     public Transform shell;
     public Transform shellEjection;
@@ -15,21 +24,57 @@ public class Gun : MonoBehaviour
 
     float nextShotTime;
 
+    bool triggerReleasedSinceLastShot;
+    int shotsRemainingInBurst;
+
     private void Start()
     {
         muzzleFlash = GetComponent<MuzzleFlash>();
+        shotsRemainingInBurst = burstCount;
     }
 
-    public void Shoot()
+    void Shoot()
     {
         if(Time.time > nextShotTime)
         {
-            nextShotTime = Time.time + msBetweenShots / 1000;
-            Projectile newProjectile = Instantiate(projectile, muzzle.position, muzzle.rotation);
-            newProjectile.setSpeed(muzzleVelocity);
+            if(fireMode == FireMode.Burst)
+            {
+                if(shotsRemainingInBurst == 0)
+                {
+                    return;
+                }
+                shotsRemainingInBurst--;
+            }
+            else if(fireMode == FireMode.Single)
+            {
+                if(!triggerReleasedSinceLastShot)
+                {
+                    return;
+                }
+            }
+
+            for(int i = 0; i < projectileSpawn.Length; i++)
+            {
+                nextShotTime = Time.time + msBetweenShots / 1000;
+                Projectile newProjectile = Instantiate(projectile, projectileSpawn[i].position, projectileSpawn[i].rotation);
+                newProjectile.setSpeed(muzzleVelocity);
+
+            }
 
             Instantiate(shell, shellEjection.position, shellEjection.rotation);
             muzzleFlash.Activate();
         }
+    }
+
+    public void OnTriggerHold()
+    {
+        Shoot();
+        triggerReleasedSinceLastShot = false;
+    }
+
+    public void OnTriggerRelease()
+    {
+        triggerReleasedSinceLastShot = true;
+        shotsRemainingInBurst = burstCount;
     }
 }
