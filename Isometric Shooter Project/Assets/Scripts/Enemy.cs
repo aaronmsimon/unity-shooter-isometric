1ad8bb1b8880053fb325a9f21fe56d5f7fcc80e3
aaronmsimon <aaronsimon@gmail.com>
2021-06-27 11:00:02 -0700

    Enemy death particles to match variable color of enemy

--- Isometric Shooter Project/Assets/Scripts/Enemy.cs
+++ Isometric Shooter Project/Assets/Scripts/Enemy.cs
@@ -70,7 +70,7 @@ public class Enemy : LivingEntity
 
         startingHealth = enemyHealth;
 
-        skinMaterial = GetComponent<Renderer>().material;
+        skinMaterial = GetComponent<Renderer>().sharedMaterial;
         skinMaterial.color = skinColor;
         originalColor = skinMaterial.color;
     }
