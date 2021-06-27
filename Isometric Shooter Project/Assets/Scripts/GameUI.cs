52e2d830e8353d18289fb10d0bb7b62eb5099f58
aaronmsimon <aaronsimon@gmail.com>
2021-06-27 11:00:43 -0700

    Wave banner

--- Isometric Shooter Project/Assets/Scripts/GameUI.cs
+++ Isometric Shooter Project/Assets/Scripts/GameUI.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
     public Image fadePlane;
     public GameObject gameOverUI;
+
+    public RectTransform newWaveBanner;
+    public Text newWaveTitle;
+    public Text newWaveEnemyCount;
+
+    Spawner spawner;
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +21,48 @@ public class GameUI : MonoBehaviour
         FindObjectOfType<Player>().OnDeath += OnGameOver;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        spawner = FindObjectOfType<Spawner>();
+        spawner.OnNewWave += OnNewWave;
+    }
+
+    void OnNewWave(int waveNumber)
+    {
+        string[] numbers = { "One", "Two", "Three", "Four", "Five" };
+        newWaveTitle.text = "- Wave " + numbers[waveNumber - 1] + " -";
+        string enemyCountString = ((spawner.waves[waveNumber - 1].infinite) ? "Infinite" : spawner.waves[waveNumber - 1].enemyCount + "");
+        newWaveEnemyCount.text = "Enemies: " + enemyCountString;
+
+        StopCoroutine("AnimateNewWaveBanner");
+        StartCoroutine("AnimateNewWaveBanner");
+    }
+
+    IEnumerator AnimateNewWaveBanner()
+    {
+        float delayTime = 1.5f;
+        float speed = 3f;
+        float animatePercent = 0;
+        int dir = 1;
+
+        float endDelayTime = Time.time + 1 / speed + delayTime;
+
+        while(animatePercent >= 0)
+        {
+            animatePercent += Time.deltaTime * speed * dir;
+
+            if(animatePercent >= 1)
+            {
+                animatePercent = 1;
+                if(Time.time > endDelayTime)
+                {
+                    dir = -1;
+                }
+            }
+
+            newWaveBanner.anchoredPosition = Vector2.up * Mathf.Lerp(-220, 0, animatePercent);
+            yield return null;
+        }
     }
 
     void OnGameOver()
@@ -42,6 +87,6 @@ public class GameUI : MonoBehaviour
     // UI Input
     public void StartNewGame()
     {
-        Application.LoadLevel("Game");
+        SceneManager.LoadScene("Game");
     }
 }
